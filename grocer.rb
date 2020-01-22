2030fd9c9825f4abe790938f19d9dc1c138a9f2d
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-22 21:55:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -80,7 +80,7 @@ def apply_clearance(cart)
   # REMEMBER: This method **should** update cart
   i = 0
   results = []
-  #binding.pry
+  binding.pry
   while i < cart.length do
     cart_clearance = cart[i][:clearance]
     cart_price = cart[i][:price]
