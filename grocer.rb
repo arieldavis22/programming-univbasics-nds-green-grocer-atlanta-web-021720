3761748394e6377beeb79fd4afafb845d717f11f
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-22 21:53:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -86,7 +86,8 @@ def apply_clearance(cart)
     cart_price = cart[i][:price]
 
     if cart_clearance
-      cart_price *= 0.8.round(2)
+      cart_price *= 0.8
+      cart_price.round(2)
     end
     i += 1
   end
